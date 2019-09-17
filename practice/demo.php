f87007d839844d80b28d5b02d1a03b6e3831a968
Chantelle D'mello <chantelle.dmello@gmail.com>
2019-09-17 17:11:58 -0400

    Project 1 HTML and CSS files

---
+++ practice/demo.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Basic Page Info
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>DGMD E-2 Project 1</title>
+  <meta name="description" content="Proof of concept for Git and Server setup">
+  <meta name="author" content="Chantelle D'mello">
+
+  <!-- Styles
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel=stylesheet href="css/normalise.css">
+  <link rel="stylesheet" href="css/styles.css">
+
+  <!-- Web Fonts
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,900|Playfair+Display:400,400i,700,700i,900,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Darker+Grotesque:300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+  <!-- Section 1 - Introduction -->
+
+  <section>
+      <div>
+        <div>
+        <img src="images/chantelle.jpg">
+      </div>
+        <h1> Chantelle D'mello </h1>
+        <p> Hi guys! My name is Chantelle and I'm an MLA in Digital Media international student living in sunny Trinidad and Tobago.</p>
+        <p> My background is in Journalism and I graduated in 2016 with a BSc in Journalism from Northwestern (go Wildcats!) but have since seen my interests take me more into the digital world than the newsroom.
+        <p>
+            I'm halfway through my degree and have basic programming knowledge via David Malan's CS50 and Lisa Diorio's WP class that touched on PHP.
+            While I'm an aspiring frontend designer, I'm stocked to take a look 'under the hood' and learn about how server-side technologies work, especially
+          if I plan to work with a CMS like WP in the future. </p>
+        <p>On the personal side, I'm a third culture kid that's spent the majority of my life in Qatar and Oman. I'm also an avid foodie, cat-lover, and gym bunny.
+          Excited to get cracking on this class and to e-meet all of you! </p>
+          <img src="images/wave.gif">
+      </div>
+  </section>
+
+</body>
