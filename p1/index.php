733c1c220136d6c7f8c4c0b29d7b986b5b1555c3
Chantelle D'mello <chantelle.dmello@gmail.com>
2019-09-17 19:56:15 -0400

    Project 1 - Updated text and CSS

---
+++ p1/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Basic Page Info
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>DGMD E-2 Project 1</title>
+  <meta name="description" content="Proof of concept for Git and Server setup">
+  <meta name="author" content="Chantelle D'mello">
+
+  <!-- Styles
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel=stylesheet href="css/normalise.css">
+  <link rel="stylesheet" href="css/styles.css">
+
+  <!-- Web Fonts
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Darker+Grotesque:300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+  <!-- Section 1 - Introduction -->
+
+  <section>
+      <div>
+        <div>
+        <img src="images/chantelle.jpg" alt="profile picture">
+      </div>
+        <h1> Chantelle D'mello </h1>
+        <p> Hi guys! My name is Chantelle and I'm an international Graduate student currently pursuing my MLA in Digital Media while living in sunny Trinidad and Tobago.</p>
+        <p> While my background is in journalism (I graduated in 2016 with a BSc in Journalism from Northwestern - go Wildcats!), I've realised
+          that my true love is in reating digital products that combine graphics with storytelling to create seamless, enriching experiences. </p>
+
+        <p>
+            I'm halfway through my degree and have basic programming knowledge via David Malan's CS50 and Lisa Diorio's WP class that touched on PHP.
+            While I'm an aspiring frontend designer, I'm stoked to take a peek 'under the hood' and learn about how server-side technologies work. I think it's
+            an extremely valuable skill to have, especially given the rapid rise of CMSs like WP. </p>
+            <p> I'm a visual thinker and learner, but I love the challenge that programming poses and the instant gratification you get when something works the way it should.
+            (rare, but well worth it!)</p>
+        <p>On the personal side,
+
+          I'm a third culture kid who's spent the majority of her life in Qatar and Oman, so feel free to ask
+          my about life in the desert. I'm an avid foodie, cat-lover, and gym bunny.</p>
+          <p>
+          Excited to get cracking on this class and to e-meet all of you! </p>
+          <img src="images/wave.gif" alt="waving hand">
+      </div>
+  </section>
+
+</body>
